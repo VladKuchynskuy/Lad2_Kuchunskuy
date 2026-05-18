55b00c06ae86f73f2d153e8df765cab50c4923bc
VladKuchynskuy <8500846@stud.kai.edu.ua>
2026-05-19 01:27:16 +0300

    break: fail unit test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const sum = (a, b) => a + b;
 console.log("Running Unit Tests...");
-if (sum(2, 2) === 4) {
+if (sum(2, 2) === 5) {
   console.log("Unit Tests Passed!");
   process.exit(0);
 } else {
